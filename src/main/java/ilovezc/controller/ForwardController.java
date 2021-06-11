678e05850800e4dd2f2f79282e5bb3fe4c2f9746
dingziming1 <737234660@qq.com>
2021-06-12 00:14:11 +0800

    feat: update tab change

--- src/main/java/ilovezc/controller/ForwardController.java
+++ src/main/java/ilovezc/controller/ForwardController.java
@@ -1,11 +1,15 @@
 package ilovezc.controller;
 
+import ilovezc.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class ForwardController {
-//    @RequestMapping("/index")
+    //    @RequestMapping("/index")
 //    public String index() {
 //        return "forward:/index.html";
 //    }
@@ -17,20 +21,25 @@ public class ForwardController {
         map.put("count", count);
         return "main";
     }
+
     @RequestMapping("/info")
     public String info() {
         return "info";
     }
+
     @RequestMapping("/updatepass")
     public String updatepass() {
         return "updatepass";
     }
+
     @RequestMapping("/activity")
     public String activity() {
         return "activity";
     }
+
     @RequestMapping("/reg")
     public String toReg() {
         return "reg";
     }
+
 }
